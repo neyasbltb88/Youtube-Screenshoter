2e364617e2332183eff9b66144ada44dc6ec8bcd
neyasbltb88 <neyasbltb_88@mail.ru>
2024-08-21 00:25:56 +0300

    Немного улучшена система инициализации скрипта

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import { insertButton } from '@utils/insertButton';
 import { capture } from '@utils/capture';
 import { hotKey } from '@utils/hotKey';
 
+/** Селектор блока-контейнера на странице, куда будет вставлена(insertAdjacentElement('afterbegin')) кнопка создания скриншотов */
+const containerSelector = '.ytp-right-controls';
+
 function main() {
-    setTimeout(insertButton, 2000);
+    let interval = setInterval(() => {
+        const result = insertButton(containerSelector);
+        // После успешной вставки кнопки останавливаем интервал
+        if (result) clearInterval(interval);
+    }, 100);
+
     hotKey(capture, 'ControlLeft', 'KeyQ');
 }
 
--- src/utils/insertButton.js
+++ src/utils/insertButton.js
@@ -1,14 +1,17 @@
 import { capture } from './capture.js';
 
-export const insertButton = () => {
-    const controls = document.querySelector('.ytp-right-controls');
-    const captureButton = document.createElement('div');
+export const insertButton = (containerSelector) => {
+    const container = document.querySelector(containerSelector);
+    if (!container) return false;
 
+    const captureButton = document.createElement('div');
     captureButton.setAttribute('class', 'ytp-button');
     captureButton.setAttribute('style', 'font-size: 1.6em; text-align: center;');
     const textnode = document.createTextNode('📷');
     captureButton.appendChild(textnode);
 
-    controls.insertAdjacentElement('afterbegin', captureButton);
+    container.insertAdjacentElement('afterbegin', captureButton);
     captureButton.addEventListener('click', capture);
+
+    return true;
 };
